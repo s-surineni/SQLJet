6261076d3ae3768950d4f3acc3f714ac5252eac9
sampath surineni <sampath.surineni@gmail.com>
2021-08-29 10:35:06 +0530

    Initial skeleton

---
+++ src/main/kotlin/com/greatest/sqljet/SqlJetApplication.kt
@@ -0,0 +1,11 @@
+package com.greatest.sqljet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SqlJetApplication
+
+fun main(args: Array<String>) {
+    runApplication<SqlJetApplication>(*args)
+}
---
+++ src/test/kotlin/com/greatest/sqljet/SqlJetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.greatest.sqljet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SqlJetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
